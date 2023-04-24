352e5911c0f0501068aa9b71089e0db0b4dfac1f
nicolo <nicolosalimbeni@gmail.com>
2023-04-24 16:45:30 +0200

    state file menagment completed

--- include/StateFile.h
+++ include/StateFile.h
@@ -2,16 +2,26 @@
 #ifndef StateFile_h
 #define StateFile_h
 
+#include <map>
 #include <string>
 #include <vector>
 
 class StateFile {
  public:
   explicit StateFile(std::string path);
-  std::string ValueOf(std::string tag);
+  std::string ValueOf(std::string key);
+  void        UpdateValueOf(std::string key, std::string value);
+  void        PrintAll();
+  void        UpdateFile();
+  bool        Contains(std::string key);
 
  private:
-  std::vector<std::string> file_info;
+  // cointains all lines, also comments
+  std::vector<std::string> all_file;
+  // contains only usefull information for the program
+  std::map<std::string, std::string> info;
+  // path to the state file
+  std::string file_path;
 };
 
 #endif
